ba40734a827549e51a020833e81287025ea0a5d0
leroyg <leroy.gardner@gmail.com>
2019-07-28 20:25:12 -0500

    fixed headerbar position relative to app content

--- better-prof/src/App.js
+++ better-prof/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { BrowserRouter as Router, Route, } from 'react-router-dom';
+import { BrowserRouter as Router,  Route, } from 'react-router-dom';
+import Header from "./components/Header";
 
 
 import "./App.css";
@@ -7,16 +8,18 @@ import "./App.css";
 import HomePage from "./components/HomePage";
 import Login from "./components/Login";
 import StudentList from "./components/StudentsList";
+import Register from "./components/Register";
 
 
 
 const App = () => {
   return (
     <div>
+      <Header/>
       <Route exact path='/' component={HomePage}/>
         <Route exact path='/login' component={Login} />
         <Route exact path='/student-list' component={StudentList} />
-      <HomePage/>
+        <Route exact path='/register' component={Register}/>
     </div>
   );
 };
--- better-prof/src/components/HomePage.js
+++ better-prof/src/components/HomePage.js
@@ -1,8 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Container from "@material-ui/core/Container";
-import AppBar from "@material-ui/core/AppBar";
-import Button from "@material-ui/core/Button";
 
 
 import StudentsList from ".//StudentsList";
@@ -10,22 +8,9 @@ import StudentsList from ".//StudentsList";
 const Home = () => {
   return (
     <Container>
-      <AppBar>
-        <div className="app-bar">
-          <h1>Better Professor</h1>
-            <div className="menu-buttons">
-              <Link to="/login">
-                <Button>Login</Button>
-              </Link>
-              <Link to="/register">
-                <Button>Register</Button>
-              </Link>
-            </div>
+        <div className="main-content">
+          <StudentsList/>
         </div>
-      </AppBar>
-      <div className="main-content">
-        <StudentsList/>
-      </div>
     </Container>
   );
 };
