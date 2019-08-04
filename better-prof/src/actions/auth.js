1e6e853f7f77f91dd3ab11d997a37962f979fe95
clay-most <46730958+clay-most@users.noreply.github.com>
2019-08-04 13:57:22 -0600

    polish auth actions

--- better-prof/src/actions/auth.js
+++ better-prof/src/actions/auth.js
@@ -1,34 +1,36 @@
-import axios from "axios";
+import axios from 'axios';
 
-export const REGISTER_START = "REGISTER_START";
-export const REGISTER_SUCCESS = "REGISTER_SUCCESS";
-export const REGISTER_ERROR = "REGISTER_ERROR";
+export const REGISTER_START = 'REGISTER_START';
+export const REGISTER_SUCCESS = 'REGISTER_SUCCESS';
+export const REGISTER_ERROR = 'REGISTER_ERROR';
 
-export const LOGIN_START = "LOGIN_START";
-export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
-export const LOGIN_ERROR = "LOGIN_ERROR";
+export const LOGIN_START = 'LOGIN_START';
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGIN_ERROR = 'LOGIN_ERROR';
 
+//Create new user action and store user token localy//
+export const login = credentials => dispatch => {
+  dispatch({ type: LOGIN_START });
+  return axios
+    .post('https://betterprofessor.herokuapp.com/api/login', credentials)
+    .then(response => {
+      localStorage.setItem('token', response.data.token);
+      dispatch({ type: LOGIN_SUCCESS });
+      return true;
+    })
 
-export const login = creds => dispatch => {
-    dispatch({type:LOGIN_START});
-    return axios.post("https://betterprofessor.herokuapp.com/api/login", creds).then(response => {localStorage.setItem('token',
-    response.data.token);
-    dispatch({type:LOGIN_SUCCESS});
-    return true;
-})
+    .catch(error => console.log(error.response));
+};
 
-    .catch(error => console.log(error.response)); 
-}
+//Register new user action//
+export const register = credentials => dispatch => {
+  dispatch({ type: REGISTER_START });
+  return axios
+    .post('https://betterprofessor.herokuapp.com/api/register', credentials)
+    .then(response => {
+      dispatch({ type: REGISTER_SUCCESS });
+      return true;
+    })
 
-export const register = creds => dispatch => {
-    dispatch({type:REGISTER_START}); 
-    return axios.post("https://betterprofessor.herokuapp.com/api/register", creds).then(response => {console.log(response);dispatch({type:REGISTER_SUCCESS});
-    return true;
-})
-
-    .catch(error => console.log(error.response)); 
-}
-
-
-
-
+    .catch(error => console.log(error.response));
+};
