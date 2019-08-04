de65071f57ec6316982f1e66072db28b4c519cdf
clay-most <46730958+clay-most@users.noreply.github.com>
2019-08-04 17:49:58 -0600

    Revert "Final polish"

--- better-prof/src/components/Register.js
+++ better-prof/src/components/Register.js
@@ -1,51 +1,36 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-
 import { register } from '../actions/auth';
 
 class Register extends Component {
-  state = {
-    username: '',
-    password: ''
-  };
+    state = {
+        username: "",
+        password: ""    }
 
-  changeHandler = event => {
-    this.setState({ [event.currentTarget.name]: event.currentTarget.value });
-  };
 
-  register = event => {
-    event.preventDefault();
-    this.props.register(this.state);
-    this.props.history.push('/login');
-  };
+    changeHandler = (e) =>{
+        this.setState({ [e.currentTarget.name]: e.currentTarget.value });
+      }; 
 
-  render() {
-    return (
-      <div>
-        <h2>Please Register Below:</h2>
-        <form onSubmit={this.register}>
-          <input
-            type="text"
-            placeholder="Username"
-            value={this.state.username}
-            name="username"
-            onChange={this.changeHandler}
-          />
-          <input
-            type="text"
-            placeholder="Password"
-            value={this.state.password}
-            name="password"
-            onChange={this.changeHandler}
-          />
-          <button>Submit</button>
-        </form>
-      </div>
-    );
-  }
+    register = (e) =>{
+        e.preventDefault();
+        this.props.register(this.state);
+        this.props.history.push("/login")
+    }
+
+    render() {
+        console.log(this.state, "from Register")
+        return (
+            <div>
+                <h2>Please Register Below:</h2>
+                <form onSubmit={this.register}>
+                    <input type="text" placeholder="Username" value={this.state.username} name="username" onChange={this.changeHandler}></input>
+                    <input type="text" placeholder="Password" value={this.state.password} name="password" onChange={this.changeHandler}></input>
+                <button>Submit</button>
+                </form>
+            </div>
+        );
+    }
 }
 
-export default connect(
-  null,
-  { register }
-)(Register);
+export default connect(null, {register} ) (Register);
