dcfde2323608cdf9a95311b385d82265ab0a0dd8
clay-most <46730958+clay-most@users.noreply.github.com>
2019-08-04 14:09:29 -0600

    Spaceing between dependancies and components

--- better-prof/src/components/Dashboard.js
+++ better-prof/src/components/Dashboard.js
@@ -1,38 +1,38 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";    
+import React from 'react';
+import { BrowserRouter as Link } from 'react-router-dom';
+
 import StudentsList from './StudentsList';
-import StudentForm from "./StudentForm"
-
-
+import StudentForm from './StudentForm';
 
 function Dashboard() {
-    const token = localStorage.getItem("token")
-    return (
-        <div>{token ?
-            
-            
-            (
-            <div>
-                <StudentForm />
-                <StudentsList />
-            </div>
-           ) 
-           
-           
-           : 
-           
-           (
-             (<div>
-                 <h3>Looks like you're not logged in.</h3>
+  const token = localStorage.getItem('token');
+  return (
+    <div>
+      {token ? (
+        <div>
+          <StudentForm />
+          <StudentsList />
+        </div>
+      ) : (
+        <div>
+          <h3>Looks like you're not logged in.</h3>
 
-                 <p>Please <Link style={{ color: "black" }} to="/register"> Register </Link> or 
-    
-                    <Link style={{ color: "black" }} to="/login"> Login
-                    </Link> </p>   
-            </div>)
-           )}</div>
-
-    )
-};
+          <p>
+            Please{' '}
+            <Link style={{ color: 'black' }} to="/register">
+              {' '}
+              Register{' '}
+            </Link>{' '}
+            or
+            <Link style={{ color: 'black' }} to="/login">
+              {' '}
+              Login
+            </Link>{' '}
+          </p>
+        </div>
+      )}
+    </div>
+  );
+}
 
 export default Dashboard;
